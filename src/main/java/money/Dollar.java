17ebae5832dc7192105672a7ae66e938c33cf9f6
NULLPE <1989leosun@gmail.com>
2022-02-28 23:54:28 +0900

    day1

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,4 +1,13 @@
 package money;
 
 public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
 }
